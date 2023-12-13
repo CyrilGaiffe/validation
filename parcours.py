74cdcad6de7bcebdb0d53dae27de628f6eb80533
Cyril Gaiffe <cyril.gaiffe@ensta-bretagne.org>
2023-12-13 10:24:18 +0100

    adding parent tracking

--- parcours.py
+++ parcours.py
@@ -1,4 +1,3 @@
-
 from collections import deque
 
 # class rootedGraph :
@@ -15,6 +14,7 @@ from collections import deque
 def parcours_en_largeur(rootedGraph, query):
     file = deque([rootedGraph.root()])
     visite = set([rootedGraph.root()])
+    parents = {}
 
     while file:
         sommet_courant = file.popleft()
@@ -23,9 +23,27 @@ def parcours_en_largeur(rootedGraph, query):
             if voisin not in visite:
                 file.append(voisin)
                 visite.add(voisin)
+                if sommet_courant in parents:
+                    parents[sommet_courant].append(voisin)
+                else :
+                    parents[sommet_courant]=[voisin]
                 if query(voisin):
-                    return visite
+                    return (visite,parent(voisin,parents,rootedGraph))
     return visite
+
+
+def parent(node,dict,rootedGraph):
+    result =[]
+    courant = node
+    while (courant!=rootedGraph.root()):
+        result.append(courant)
+        for cle,valeur in dict.items():
+            for i in range(len(valeur)):
+                if valeur[i] == courant:
+                    courant = cle
+                
+    return result
+    
 
 # G = rootedGraph({'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F'], 'D': ['B'], 'E': ['B', 'F'], 'F': ['C', 'E']}, 'A')
 # print(parcours_en_largeur(G))
@@ -65,7 +83,9 @@ class hanoiRG:
                         disk = new_state.towers[source].pop(0)
                         if not new_state.towers[target] or new_state.towers[target][0] > disk:
                             new_state.towers[target].insert(0,disk)
+                            
                             voisins.append(new_state)
+
         return voisins
     
     def isFinalState(self,hanoiState):
@@ -74,5 +94,9 @@ class hanoiRG:
 
 h = hanoiRG()
 p = parcours_en_largeur(h, h.isFinalState)
-for state in p:
+for state in p[0]:
     print(state.towers)
+print("Solution branch is :\n")
+for state in p[1]:
+    print(state.towers)
+#TODO ajouter la gestion de parents
