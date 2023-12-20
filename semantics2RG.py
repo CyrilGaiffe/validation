2f48352e46a9c3774bf3f650ef22ff9d42920f83
Cyril Gaiffe <cyril.gaiffe@ensta-bretagne.org>
2023-12-20 11:05:40 +0100

    semantic2rg

--- semantics2RG.py
+++ semantics2RG.py
@@ -1,8 +1,16 @@
-from parcours import hanoiRG
+from parcours import RootedGraph
 
 class semantic2RG:
     def __init__(self,semantic):
         self.semantic=semantic
     
-    def semantic2rg(self):
-        hanoi = hanoiRG()
+    def root(self):
+        return self.semantic.initial()
+    
+    def neighbors(self, state):
+        actions = self.semantic.actions(state)
+        voisins = []
+        for action in actions:
+            targets = self.semantic.execute(action,state)
+            voisins.extend(targets)
+
